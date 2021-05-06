2d6bbab4301d814de7064c5849b5a55381c1bd6c
Jamison Griffith <jamison.griffith@gmail.com>
2021-05-06 09:34:20 -0600

    Add new method to tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,9 @@
 class TasksController < ApplicationController
   def index
-    @tasks = ['Task 1', 'Task 2', 'Task 3']
+    @tasks = ['Task 1', 'Task 2', 'Task 3', 'Do the Laundry']
   end
+
+  def new
+  end
+
 end
