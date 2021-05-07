49138b39efec9519a428a683c55bc052d64ee34d
Jamison Griffith <jamison.griffith@gmail.com>
2021-05-06 20:12:35 -0600

    Add edit and delete task functionality

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,15 +7,34 @@ class TasksController < ApplicationController
   end
 
   def create
-    binding.pry
     task = Task.new({
       title: params[:task][:title],
       description: params[:task][:description]
     })
-  binding.pry
     task.save
-    binding.pry
     redirect_to '/tasks'
   end
 
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update({
+      title: params[:task][:title],
+      description: params[:task][:description]
+    })
+    task.save
+    redirect_to "/tasks/#{task.id}"
+  end
+
+  def destroy
+    Task.destroy(params[:id])
+    redirect_to '/tasks'
+  end
 end
